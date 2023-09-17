42b1111f167da5609cb2c0fdcb6f3d5c40013ad6
Diego Ramos <diegorramos84@gmail.com>
2023-09-17 18:13:42 +0100

    add navbar

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'django_vite_plugin'
 ]
 
 MIDDLEWARE = [
